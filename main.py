c575f6e1e5285a12f208993339616ed36a430de1
Amin Zeina <36240585+AminZeina@users.noreply.github.com>
2024-10-18 13:40:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from typing import Annotated
+from ultralytics import YOLO
+
+from fastapi import FastAPI, File, UploadFile
+from datetime import datetime
+import logging
+import uvicorn
+
+'''
+This is an inital "mock" endpoint for uploading an image for object detection.
+The /detect endpoint is used to upload an image, which is then passed through lightly-trained
+YOLO model for drones, then returns a json response containing the list of detected objects in 
+the image.
+
+Currently, no opitimization is being done and I'm simply using the ultralytics .predict() function.
+It's worth noting that the detection itself only typically takes 30-40 ms (shown by result.speed below),
+so the majority of time is spend on overhead. So, this likely can and will be improved on in the future.
+'''
+
+logger = logging.getLogger('uvicorn')
+
+app = FastAPI()
+
+model = YOLO("yolo_initial.pt")
+
+
+@app.post("/detect")
+async def create_upload_file(file: UploadFile):
+    logger.info('Detection endpoint hit')
+    start = datetime.now()
+
+    logger.info(file.filename)
+    logger.info(file.size)
+    logger.info(file.headers)
+    
+
+    file_location = f"uploaded/{file.filename}"
+    with open(file_location, "wb+") as f:
+        f.write(file.file.read())
+
+    results = model.predict(file_location)
+
+    result = results[0]
+    result.save(f'detected/out-{file.filename}')    
+    
+    elapsed = datetime.now() - start
+    logger.info(f'elapsed time: {elapsed.total_seconds() * 1000}')
+    logger.info(f'results.names: {result.names}')
+    logger.info(f'results.speed (in ms): {result.speed}')
+
+    return result.to_json()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080,reload=True)
---
+++ testRequests.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from datetime import datetime
+from pathlib import Path
+
+
+url = "http://127.0.0.1:5000/predictionImages" # using burnsy's inital endpoints
+#url = "http://127.0.0.1:8080/detect"
+
+img_dir = "test_images"
+elapsed_time_list = []
+
+pathlist = Path(img_dir).rglob('*')
+for img_path in pathlist:
+    with open(img_path, 'rb') as img:
+        name_img= os.path.basename(img_path)
+        files= {'file': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
+
+        session = requests.Session()
+        start_t = datetime.now()
+        r = session.post(url,files=files)
+        elapsed = (datetime.now() - start_t).total_seconds() * 1000 # in ms
+        print(r.status_code)
+
+        try:
+            print(r.json())
+        except:
+            with open("flask_response.png", 'wb') as f:
+                f.write(r.content)
+        print(f'elapsed time: {elapsed} ms')
+        elapsed_time_list.append(elapsed)
+
+print("*** tests done ***")
+print(f'list of elapased time per request {elapsed_time_list}')
+
+
+'''
+Using the faster-RCNN model (burnsy's) endpoints, a full request to response takes roughly 1800 ms for a new image, and then roughly 500 ms for subsequent requests 
+
+Using the YOLO model (main.py in this directory), the same tests take roughly 800ms and then 100ms (for a new image and then subsequent requests, respectively)
+'''
